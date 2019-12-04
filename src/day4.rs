1e91887221d50e9690fa1712099254233ed1070f
Thomas Kendrick <tommkendrick@gmail.com>
2019-12-04 20:28:34 +0000

    My solution is bad and I feel bad.

--- src/day4.rs
+++ src/day4.rs
@@ -1,11 +1,11 @@
 #[aoc(day4, part1)]
-fn solve_part_1(string: &str) -> i32 {
-    let range_bounds: Vec<i32> = string.split("-").map(|r| r.parse().unwrap()).collect();
+fn solve_part_1(string: &str) -> usize {
+    let range_bounds: Vec<usize> = string.split("-").map(|r| r.parse().unwrap()).collect();
 
     let mut number_acceptable_values = 0;
 
     for value in range_bounds[0]..range_bounds[1] {
-        let mut prev_value: i32 = 0;
+        let mut prev_value: usize = 0;
         let mut has_pair = false;
         let is_increasing = num_digits(value).iter().fold(true, |acc, x| {
             let mut does_increase = false;
@@ -25,40 +25,39 @@ fn solve_part_1(string: &str) -> i32 {
     return number_acceptable_values;
 }
 #[aoc(day4, part2)]
-fn solve_part_2(string: &str) -> i32 {
-    let range_bounds: Vec<i32> = string.split("-").map(|r| r.parse().unwrap()).collect();
+fn solve_part_2(string: &str) -> usize {
+    let range_bounds: Vec<usize> = string.split("-").map(|r| r.parse().unwrap()).collect();
 
     let mut number_acceptable_values = 0;
 
     for value in range_bounds[0]..range_bounds[1] {
-        let mut prev_value: i32 = 0;
-        let mut has_pair = false;
-        let mut existing_pair_groups: Vec<(i32, i32)> = Vec::new();
+        let mut prev_value: usize = 0;
+        let mut counts: [usize; 10]= [0,0,0,0,0,0,0,0,0,0];
         let is_increasing = num_digits(value).iter().fold(true, |acc, x| {
             let mut does_increase = false;
-            if *x == prev_value && existing_pair_groups.contains(&x,_) {
-
-            } else if *x == prev_value {
-                has_pair = true;
+            if *x == prev_value && counts[*x] == 2  {
                 does_increase = true;
-                existing_pair_groups.push((*x, 1))
-            } else if x > &prev_value {
+                counts[*x] += 1;
+            } else if *x == prev_value && counts[*x] == 0{
+                does_increase = true;
+                counts[*x] = 2
+            } else if x >= &prev_value {
                 does_increase = true;
             }
             prev_value = *x;
             return does_increase && acc;
         });
-        if is_increasing && has_pair {
+        if is_increasing && counts.contains(&2) {
             number_acceptable_values += 1;
         }
     }
     return number_acceptable_values;
 }
-pub fn num_digits(num: i32) -> Vec<i32> {
+pub fn num_digits(num: usize) -> Vec<usize> {
     num.to_string()
         .chars()
         .filter_map(|x| x.to_digit(10))
-        .map(|x| (x as i32))
+        .map(|x| (x as usize))
         .collect()
 }
 // #[aoc(day4, part2)]
